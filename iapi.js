6aefbfca71d69bc196bb3f5e9164d5b1b3cef6d1
s2 <s@ireoo.com>
2016-11-16 00:20:16 +0800

    init

--- iapi.js
+++ iapi.js
@@ -1,24 +1,30 @@
 /**
  * Created by ireoo on 16-11-15.
  */
-var needle  = require('needle');
+var needle = require('needle');
 
 needle.defaults({
     open_timeout: 5000,
     user_agent: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0'
 });
 
-module.exports = function (url, data, callback) {
-    needle.post(url, JSON.stringify(data), function(err, res) {
-        if(!err) {
-            try {
-                var json = JSON.parse(res.body);
-                callback(null, json);
-            }catch (e) {
-                callback(e, null);
+module.exports = function(url, data, callback) {
+    if (url == '') url = "http://api.daoapp.io";
+    if (!data || data == [] || typeof data != object) callback("data or json!!!", null);
+    try {
+        needle.post(url, JSON.stringify(data), function(err, res) {
+            if (!err) {
+                try {
+                    var json = JSON.parse(res.body);
+                    callback(null, json);
+                } catch (e) {
+                    callback(e, null);
+                }
+            } else {
+                callback(err, null);
             }
-        } else {
-            callback(err, null);
-        }
-    });
+        });
+    } catch (e) {
+        callback(e, null);
+    }
 };
