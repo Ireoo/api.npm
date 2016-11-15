446b545dfa594defae06d4f78d1413e4c2055f0c
s2 <s@ireoo.com>
2016-11-16 00:43:43 +0800

    init

--- iapi.js
+++ iapi.js
@@ -8,7 +8,7 @@ needle.defaults({
     user_agent: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0'
 });
 
-module.exports = function(data, callback) {
+exports.api = function(data, callback) {
     if (!data || data == [] || typeof data != object) callback("data or json!!!", null);
     try {
         needle.post("http://api.daoapp.io", JSON.stringify(data), function(err, res) {
