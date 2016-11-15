903be0d84873b3b881191b4b8b3739553adb4797
S2 <s@ireoo.com>
2016-11-16 04:22:47 +0800

    优化程序

--- iapi.js
+++ iapi.js
@@ -10,13 +10,14 @@ needle.defaults({
 
 module.exports = {
     config: {
+        url: "http://api.daoapp.io/",
         key: ""
     },
 
     api: function(url, data, callback) {
         try {
             data.key = this.config.key;
-            needle.post("http://api.daoapp.io/" + url, JSON.stringify(data), function(err, res) {
+            needle.post(this.config.url + url, JSON.stringify(data), function(err, res) {
                 if (!err) {
                     try {
                         // var json = JSON.parse(res.body);
